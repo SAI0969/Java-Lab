9c5addbefbdd32bfa8143720038076e687dd2a31
SAI0969 <asm1scs590526@gmail.com>
2025-07-19 17:03:53 +0530

    Create Arthimetic.java

---
+++ Arthimetic.java
@@ -0,0 +1,24 @@
+class Arthimetic{
+  public static void main(String args[]){
+    int a = 8, b = 4;
+    int c = a / b;
+    int d = c * a;
+    int e = b + d;
+    int f = d % c;
+    int g = f  - e;
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(e);
+  }
+}
+
+// Output
+// 2
+// 16
+// 20
+// 0
+// -20
+
+
