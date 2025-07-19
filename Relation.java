312b36671e0bb165f737ecc458090d1ccb74bba7
SAI0969 <asm1scs590526@gmail.com>
2025-07-19 17:08:54 +0530

    Create Relation.java

---
+++ Relation.java
@@ -0,0 +1,19 @@
+class Relation{
+  public static void main(String args[]){
+    int a = 5, b = 6, c = 4, d = 8;
+    System.out.println(a > b);
+    System.out.println(a < b);
+    System.out.println(c > d);
+    System.out.println(c < d);
+    System.out.println(a == b);
+    System.out.println(c == d);
+  }
+}
+
+// output
+// false
+// true
+// false
+// true
+// false
+// false
