e520d56a86f2e4504971a38265b641a6a707aeb4
SAI0969 <asm1scs590526@gmail.com>
2025-07-26 16:28:17 +0530

    Create Controlflow3.java

---
+++ Assignment2(Control flows)/Controlflow3.java
@@ -0,0 +1,26 @@
+class Controlflow3{
+	public static void main(String args[]){
+		if (x > 10){
+			3 * scale;
+		}
+		else {
+			4 * scale;
+		}
+
+		if (income > 10000){
+			income * 0.2;
+		}
+		else{
+			income * 0.17 + 1000;
+		}
+
+		if (number % 3 == 0){
+			System.out.print("i");
+		}
+		else{
+			System.out.print("j");
+		}
+
+
+	}
+}
