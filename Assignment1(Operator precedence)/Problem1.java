1212ea2af81dfdebca888cfd02a6b54bd2847f84
SAI0969 <asm1scs590526@gmail.com>
2025-07-26 16:35:28 +0530

    Create Problem1.java

---
+++ Assignment1(Operator precedence)/Problem1.java
@@ -0,0 +1,13 @@
+class Problem1{
+  public static void main(String args[]){
+    int a  = 2 + 3 * 4;
+    System.out.println(a);
+  }
+}
+
+
+Output ->>
+12
+
+* has higher percedence than +
+  3 * 4 = 12 + 2 = 14
