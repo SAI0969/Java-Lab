6a1623a932351f1044851330b1fb8c8cb5f98fb9
SAI0969 <asm1scs590526@gmail.com>
2025-07-27 09:47:59 +0530

    Update Controlflow4.java

--- Assignment2(Control flows)/Controlflow4.java
+++ Assignment2(Control flows)/Controlflow4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FutureDayWithSwitch {
+public class Controlflow4{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
