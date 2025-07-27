cb4f1bca6fe36b8f91fada8966f436c88c9c74c7
SAI0969 <asm1scs590526@gmail.com>
2025-07-27 09:47:26 +0530

    Update Controlflow4.java

--- Assignment2(Control flows)/Controlflow4.java
+++ Assignment2(Control flows)/Controlflow4.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
-public class Controlflow4{
+
+public class FutureDayWithSwitch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter today's day:  ");
+        System.out.print("Enter today's day (0=Sun, 1=Mon, ..., 6=Sat): ");
         int today = sc.nextInt();
 
         System.out.print("Enter the number of days elapsed since today: ");
-        int elapsedDays = sc.nextInt();
+        int elapsed = sc.nextInt();
 
-        int futureDay = (today + elapsedDays) % 7;
+        int future = (today + elapsed) % 7;
 
-        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        String todayName = "", futureName = "";
 
-        System.out.println("Today is " + days[today] + " and the future day is " + days[futureDay]);
+        switch (today) {
+            case 0: todayName = "Sunday"; break;
+            case 1: todayName = "Monday"; break;
+            case 2: todayName = "Tuesday"; break;
+            case 3: todayName = "Wednesday"; break;
+            case 4: todayName = "Thursday"; break;
+            case 5: todayName = "Friday"; break;
+            case 6: todayName = "Saturday"; break;
+            default: todayName = "Invalid day"; break;
+        }
+
+        switch (future) {
+            case 0: futureName = "Sunday"; break;
+            case 1: futureName = "Monday"; break;
+            case 2: futureName = "Tuesday"; break;
+            case 3: futureName = "Wednesday"; break;
+            case 4: futureName = "Thursday"; break;
+            case 5: futureName = "Friday"; break;
+            case 6: futureName = "Saturday"; break;
+        }
+
+        if (!todayName.equals("Invalid day")) {
+            System.out.println("Today is " + todayName + " and the future day is " + futureName);
+        } else {
+            System.out.println("Invalid input for today's day.");
+        }
 
         sc.close();
     }
